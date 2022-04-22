e961cc10b6db80f8fe2e7ddbdf47696460e22e1d
Abhishek <95518276+Abhishek0075@users.noreply.github.com>
2022-04-22 11:34:57 +0530

    Update pygamebasic.py

--- pygamebasic.py
+++ pygamebasic.py
@@ -2,12 +2,15 @@ import pygame
 pygame.init
 win=pygame.display.set_mode((500,500))
 pygame.display.set_caption("Basics")
-
+x,y=450,450
 run=True
 while(run==True):
-	pygame.time.delay(100)
+	pygame.time.delay(50)
 	for event in pygame.event.get():
 		if(event.type==pygame.QUIT):
 			run=False
+	pygame.draw.rect(win,(0,0,255),(x,y,70,70))
+	pygame.display.update()
+	
 pygame.quit()
 	

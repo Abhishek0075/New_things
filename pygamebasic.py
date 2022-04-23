c933bf1aa0e44fc195e052d0858d5403e982574b
Abhishek P <abhirajp075@gmail.com>
2022-04-23 11:04:34 +0530

    changes

--- pygamebasic.py
+++ pygamebasic.py
@@ -6,7 +6,6 @@ x,y=250,250
 vel=5
 run=True
 while(run==True):
-	pygame.draw.rect(win,(255,0,0),(100,20,70,70))
 	pygame.display.update()
 	pygame.time.delay(50)
 	for event in pygame.event.get():
@@ -15,14 +14,17 @@ while(run==True):
 	keys=pygame.key.get_pressed()
 	if keys[pygame.K_LEFT]:
 		pygame.draw.rect(win,(255,0,0),(x,y,70,70))
+		pygame.display.update()
 		x -= vel
 	if keys[pygame.K_RIGHT]:
+		pygame.draw.rect(win,(255,255,0),(x,y,70,70))
 		x += vel
 	if keys[pygame.K_UP]:
+		pygame.draw.rect(win,(255,255,255),(x,y,70,70))
 		y -= vel
 	if keys[pygame.K_DOWN]:
+		pygame.draw.rect(win,(255,100,0),(x,y,70,70))
 		y += vel
-	win.fill((255,255,255))
 	pygame.draw.rect(win,(0,0,255),(x,y,70,70))
 	pygame.display.update()
 	

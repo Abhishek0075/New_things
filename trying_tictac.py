7452dcf9a2fc3ff9572f61f3fc7923054ea5c08a
Abhishek P <abhirajp075@gmail.com>
2022-05-16 21:51:32 +0530

    Update trying_tictac.py

--- trying_tictac.py
+++ trying_tictac.py
@@ -1,25 +1,46 @@
-import pygame
-pygame.init
-win=pygame.display.set_mode((500,500))
-pygame.display.set_caption("Basics")
-vel=5
-run=True
-while(run==True):
-    pygame.display.update()
-    pygame.time.delay(50)
-    for event in pygame.event.get():
-        if(event.type==pygame.QUIT):
-            run=False
-    pygame.draw.rect(win,(0,0,255),(0,0,140,140))
-    pygame.draw.rect(win,(0,0,255),(180,0,140,140))
-    pygame.draw.rect(win,(0,0,255),(360,0,140,140))
-    pygame.draw.rect(win,(0,0,255),(0,180,140,140))
-    pygame.draw.rect(win,(0,0,255),(180,180,140,140))
-    pygame.draw.rect(win,(0,0,255),(360,180,140,140))
-    pygame.draw.rect(win,(0,0,255),(0,360,140,140))
-    pygame.draw.rect(win,(0,0,255),(180,360,140,140))
-    pygame.draw.rect(win,(0,0,255),(360,360,140,140))
-    pygame.display.update()
-	
-pygame.quit()
-	
+import numpy as np
+from random import randint as Rint
+
+
+
+def PCA(A):
+    print("INPUT MATRIX : ")
+    print(A)
+    #Get the mean values of each column
+    Mean = np.mean(A.T,axis = 1)
+    print("MEAN OF EACH COLUMN : "  )
+    print(Mean)
+    #Centering the columns
+    Center = A - Mean
+    print("CENTERING THE MATRIX : ")
+    print(Center)
+    #Calculating the covaraince matrix
+    V = np.cov(Center.T)
+    print("COVARIANCE MATRIX : ")
+    print(V)
+    #Eigendecomposition of covariance matrix
+    values,vectors = np.linalg.eig(V)
+    print("EIGENVECTORS")
+    print(vectors)
+    print("EIGENVALUES")
+    print(values)
+    print("TAKING DOT PRODUCT AND PROJECTION")
+    P = vectors.T.dot(Center.T)
+    proj = (vectors.T[:][:2]).T
+    print(proj)
+    # projecting the data
+    P = proj.T.dot(Center.T)
+    return P
+
+def main():
+    #Generating a random 2x2 matrix
+    M = np.random.randint(-10,10,size = (2,2))
+    P = PCA(M)
+    print("RESULT : ")
+    print(P)
+    print("ROUNDING : ")
+    for i in P :
+        for j in i:
+            print(round(j,2),end="\t")
+        print("\n")
+main()
